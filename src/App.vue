9bef33b1f04dac21d153f275a1df97de776ea9d3
Óscar Montañés <om@thehotelsnetwork.com>
2022-01-18 09:24:57 +0100

    chore: base project & config files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<template>
+  <header class="p-4">
+    <div class="flex justify-between menu">
+      <a href="#">Home</a>
+      <a href="#">Rooms</a>
+      <a href="#">Restaurant</a>
+      <a href="#">Weedings</a>
+      <a href="#">Membership</a>
+      <a href="#">Contact</a>
+    </div>
+  </header>
+</template>
+<style scoped>
+  .menu > a:nth-child(3) {
+    margin-right: 5rem;
+  }
+  .menu > a:nth-child(4) {
+    margin-left: 5rem;
+  }
+</style>
